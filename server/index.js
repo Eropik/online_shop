f74ad1f810333e747fec6d2e20f0aded2e348104
egor2004super@hotmail.com <egor2004super@hotmail.com>
2024-05-24 20:40:30 +0300

    Client

--- server/index.js
+++ server/index.js
@@ -1,3 +1,33 @@
+// require('dotenv').config()
+// const express = require('express')
+// const sequelize = require('./db')
+// const models=  require('./models/models')
+// const cors= require('cors')
+
+// const PORT = process.env.PORT || 5000
+
+// const app = express()
+// app.use(cors())
+// app.use(express.json())
+
+
+// //app.get('/',(req,res)=>{res.status(200).json({messege:'TI LOH'})})
+
+// const start = async()=>{
+//     try{
+
+//         await sequelize.authenticate()
+//         await sequelize.sync()
+//         app.listen(PORT, ()=>console.log(`Server started on PORT ${PORT}`))
+
+
+//     }catch(e)
+//     {console.log(e)}
+// } 
+
+// start()
+
+
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
@@ -32,4 +62,5 @@ const start = async()=>{
 } 
 
 start()
+
 
